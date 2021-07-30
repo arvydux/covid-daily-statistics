1be05ab31c91a84dd2fc91a41adfc66f3eeca052
Arvydas Kavaliauskas <arvydas.kavaliauskas83@gmail.com>
2021-07-30 16:00:32 +0300

    Route 'web' updated

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CovidDataController;
 
-Route::get('/', [CovidDataController::class, 'refreshTable'])->name('refreshData');;
-Route::get('/update', [CovidDataController::class, 'updateData'])->name('updateData');;
+Route::get('/', [CovidDataController::class, 'showCovidData'])->name('showCovidData');;
