bd62d8cbce21b7aaadfc4dbb0ff5a8cbe62419bb
Arvydas Kavaliauskas <arvydas.kavaliauskas83@gmail.com>
2021-08-02 21:25:13 +0300

    Unit tests are updated

--- tests/Unit/CovidStatisticServiceTest.php
+++ tests/Unit/CovidStatisticServiceTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit;
 
-use Carbon\Carbon;
+use App\Services\CovidStatisticService;
+use Illuminate\Http\Request;
 use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Client;
+
 
 class CovidStatisticServiceTest extends TestCase
 {
@@ -18,59 +20,25 @@ class CovidStatisticServiceTest extends TestCase
 
     public function test_data_fetched_correctly_from_form_by_default_request()
     {
+        $request = new Request();
         $request['from'] = null;
         $request['to'] = null;
         $request['country'] = null;
-
-        if ($request['from']) {
-            $from = Carbon::createFromFormat('Y-m-d', $request['from'])->startOfDay()->toIso8601ZuluString();
-        } else {
-            $from = Carbon::today()->subMonth(1)->startOfDay()->toIso8601ZuluString();;
-        }
-
-        if ($request['to']) {
-            $to = Carbon::createFromFormat('Y-m-d', $request['to'])->startOfDay()->toIso8601ZuluString();
-        } else {
-            $to = Carbon::today()->startOfDay()->toIso8601ZuluString();;
-        }
-
-        if (!$request['country']) {
-            $request['country'] = 'lithuania';
-        }
-
-        $httpClient = new Client();
-        $request = $httpClient->get("https://api.covid19api.com/country/${request['country']}?from=${from}&to=${to}");
-        $response = json_decode($request->getBody()->getContents());
-        $this->assertGreaterThanOrEqual(28, count($response));
-        $this->assertLessThanOrEqual(31, count($response));
+        $covidStatisticService = new CovidStatisticService();
+        $response = $covidStatisticService->getDataByRequest($request);
+        $this->assertGreaterThanOrEqual(28, count($response['countryData']));
+        $this->assertLessThanOrEqual(31, count($response['countryData']));
     }
 
     public function test_data_fetched_correctly_from_form_by_custom_request()
     {
+        $request = new Request();
         $request['from'] = "2021-07-01";
         $request['to'] = "2021-07-04";
         $request['country'] = 'france';
-
-        if ($request['from']) {
-            $from = Carbon::createFromFormat('Y-m-d', $request['from'])->startOfDay()->toIso8601ZuluString();
-        } else {
-            $from = Carbon::today()->subMonth(1)->startOfDay()->toIso8601ZuluString();;
-        }
-
-        if ($request['to']) {
-            $to = Carbon::createFromFormat('Y-m-d', $request['to'])->startOfDay()->toIso8601ZuluString();
-        } else {
-            $to = Carbon::today()->startOfDay()->toIso8601ZuluString();;
-        }
-
-        if (!$request['country']) {
-            $request['country'] = 'lithuania';
-        }
-
-        $httpClient = new Client();
-        $request = $httpClient->get("https://api.covid19api.com/country/${request['country']}?from=${from}&to=${to}");
-        $response = json_decode($request->getBody()->getContents());
-        $this->assertEquals('France', $response[0]->Country);
-        $this->assertEquals('2021-07-01T00:00:00Z', $response[0]->Date);
+        $covidStatisticService = new CovidStatisticService();
+        $response = $covidStatisticService->getDataByRequest($request);
+        $this->assertEquals('France', $response['countryData'][0]->Country);
+        $this->assertEquals('2021-07-01T00:00:00Z', $response['countryData'][0]->Date);
     }
 }
